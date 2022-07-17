b3136b70705c8f4f367d0bae8391e42352000400
chanintorndti <chanintorn.dti@gmail.com>
2022-07-17 16:19:44 +0700

    install package and create ui

---
+++ lib/views/your_email_ui.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class YourEmail extends StatefulWidget {
+  const YourEmail({Key? key}) : super(key: key);
+
+  @override
+  State<YourEmail> createState() => _YourEmailState();
+}
+
+class _YourEmailState extends State<YourEmail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber,
+    );
+  }
+}
